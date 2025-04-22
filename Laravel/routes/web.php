2b6c835cd99d7218c58465cdb476b860b8c0ec11
Ubai1234 <irfanubaidillah123@gmail.com>
2025-04-22 13:53:00 +0700

    setup
    setup

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProdukController;
 
-Route::get('/produkAja', [ProdukController::class, 'index']);
-
-
 // ==================1==================
 // Tambahkan route GET ke /dashboard yang memanggil method index() dari DashboardController
 
